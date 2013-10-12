218fedf2eee2901354b91b159f7c544b661d0600
Philip Stitzer <pghrpg@gmail.com>
2013-10-12 18:00:34 -0400

    has relationship model and follow buttons but not follow index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
+
+
+
+
   attr_accessible :email, :name, :password, :password_confirmation, :remember_token
   before_create :create_remember_token
   before_save { self.email = email.downcase}
@@ -19,6 +30,20 @@ class User < ActiveRecord::Base
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id) #wouldn't this return a relationship object, not a boolean?
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
 
